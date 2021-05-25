52803c5e9a79ea41404aecb5a57c3d58ca222cec
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-05-25 23:06:32 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2, t;
+            Console.WriteLine("************* FARUK SECURİTY *************");
+            Console.Write("1. Sayıyı Giriniz : ");
+            s1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2. Sayıyı Giriniz : ");
+            s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("************* FARUK SECURİTY *************");
+            t = s1 * s2;
+            Console.WriteLine(s1 + " * " + s2 + " = " + t);
+            Console.WriteLine("************* FARUK SECURİTY *************");
+            Console.ReadKey();
+
+
+        }
+    }
+}
